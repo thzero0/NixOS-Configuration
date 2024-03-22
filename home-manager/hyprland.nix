c38ccdf1a05270387425dde8442dd38b935c4c2e
thzero0 <thiagozeroa@gmail.com>
2024-03-22 03:10:15 +0000

    hyprl settings fixed

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -19,15 +19,15 @@ in
        
 
        exec-once = "asztal";
-       env = XCURSOR_SIZE,24;
+       env = "XCURSOR_SIZE,24";
       "$mainMod" = "SUPER";
 
       input = {
-        kb_layout = br;
+        kb_layout = "br";
         follow_mouse = 2;
-        touchpad {
-          natural_scroll = no;
-        }
+        touchpad = {
+          natural_scroll = "no";
+        };
       };
 
 
@@ -35,10 +35,10 @@ in
         gaps_in = 4;
         gaps_out = 15;
         border_size = 2;
-        col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg;
-        active_border = rgba(595959aa);
+        "col.active_border" = " rgba(33ccffee) rgba(00ff99ee) 45deg";
+        # active_border = " rgba(595959aa)";
 
-        layout = dwindle;
+        layout = "dwindle";
         allow_tearing = false;
       };
 
@@ -50,34 +50,36 @@ in
           size = 4;
           passes = 4;
           new_optimizations = true;
-        }
+        };
 
-        drop_shadow = yes;
+        drop_shadow = "yes";
         shadow_range = 4;
         shadow_render_power = 3;
-        col.shadow = rgba(1a1a1aee);
+        "col.shadow" = "rgba(1a1a1aee)";
       };
 
 
       animations = {
-        enabled = yes;
+        enabled = "yes";
 
         # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-        bezier = myBezier, 0.05, 0.9, 0.1, 1.05;
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
 
-        animation = windows, 1, 7, myBezier;
-        animation = windowsOut, 1, 7, default, popin 80%;
-        animation = border, 1, 10, default;
-        animation = borderangle, 1, 8, default;
-        animation = fade, 1, 7, default;
-        animation = workspaces, 1, 6, default;
+        animation = [
+        "windows, 1, 7, myBezier"
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "borderangle, 1, 8, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 6, default"
+        ];
       };
 
 
       dwindle = {
         # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-        pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-        preserve_split = yes # you probably want this
+        pseudotile = "yes"; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = "yes"; # you probably want this
       };
 
 
@@ -87,7 +89,7 @@ in
 
 
       gestures = {
-        workspace_swipe = off;
+        workspace_swipe = "off";
       };
 
 
@@ -96,62 +98,62 @@ in
       };
 
 
-      bind = {
-        "$mainMod, Return, exec, kitty";
-        "$mainMod, Q, killactive";
-        "$mainMod, M, exit";
-        "$mainMod, E, exec, dolphin";
-        "$mainMod, V, togglefloating";
-        "$mainMod, R, exec, wofi --show drun";
-        "$mainMod, P, pseudo";
-        "$mainMod, J, togglesplit";
-        "$mainMod, B, exec, firefox";
+      bind = [
+        "$mainMod, Return, exec, kitty"
+        "$mainMod, Q, killactive"
+        "$mainMod, M, exit"
+        "$mainMod, E, exec, dolphin"
+        "$mainMod, V, togglefloating"
+        "$mainMod, R, exec, wofi --show drun"
+        "$mainMod, P, pseudo"
+        "$mainMod, J, togglesplit"
+        "$mainMod, B, exec, firefox"
 
         # move focus with mainMod + arrow keys 
-        "$mainMod, left, movefocus, l";
-        "$mainMod, right, movefocus, r";
-        "$mainMod, up, movefocus, u";
-        "$mainMod, down, movefocus";
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus"
 
         # switch workspaces with mainMod + [0-9]
-        "$mainMod, 1, workspace, 1";
-        "$mainMod, 2, workspace, 2";
-        "$mainMod, 3, workspace, 3";
-        "$mainMod, 4, workspace, 4";
-        "$mainMod, 5, workspace, 5";
-        "$mainMod, 6, workspace, 6";
-        "$mainMod, 7, workspace, 7";
-        "$mainMod, 8, workspace, 8";
-        "$mainMod, 9, workspace, 9";
-        "$mainMod, 0, workspace, 10"; 
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10" 
 
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mainMod SHIFT, 1, movetoworkspace, 1";
-        "$mainMod SHIFT, 2, movetoworkspace, 2";
-        "$mainMod SHIFT, 3, movetoworkspace, 3";
-        "$mainMod SHIFT, 4, movetoworkspace, 4";
-        "$mainMod SHIFT, 5, movetoworkspace, 5";
-        "$mainMod SHIFT, 6, movetoworkspace, 6";
-        "$mainMod SHIFT, 7, movetoworkspace, 7";
-        "$mainMod SHIFT, 8, movetoworkspace, 8";
-        "$mainMod SHIFT, 9, movetoworkspace, 9";
-        "$mainMod SHIFT, 0, movetoworkspace, 10";
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
       
         # Scroll through existing workspaces with mainMod + scroll
-        "$mainMod, mouse_down, workspace, e+1";
-        "$mainMod, mouse_up, workspace, e-1";
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
 
-      };
+      ];
 
 
-      bindm = {
-        "$mainMod, mouse:272, movewindow";
-        "$mainMod, mouse:273, resizewindow";
-      };
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
 
     };
     
-  }
+  };
 
 }
 
